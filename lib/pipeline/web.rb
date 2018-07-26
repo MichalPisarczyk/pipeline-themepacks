00c816112c5bc3907cc946d2d5e6d3d66b95e1ae
Stuart Harland <s.harland@livelinktechnology.net>
2018-07-26 15:17:54 +0100

    Add Web Enqueue Tab

--- lib/pipeline/web.rb
+++ lib/pipeline/web.rb
@@ -8,11 +8,32 @@ module Pipeline
 
     def self.registered(app)
       app.get('/tasks') { pipeline_view(:tasks) }
+
+      app.get('/entries') { pipeline_view(:entry) }
+
+      app.post('/queue-tp') do
+        session[:message] = nil
+        Resque.enqueue_to('prepare_themepack', PrepareThemepack, job_spec)
+        session[:message] = "You queued a render for: #{theme_to_queue}"
+        redirect '/entries'
+      end
+
       app.tabs << 'Tasks'
 
       app.helpers do
         def pipeline_view(filename, options = {}, locals = {})
           erb(File.read(File.join(::Pipeline::Web::VIEW_PATH, "#{filename}.erb")), options, locals)
+        end
+
+        def theme_to_queue
+          params[:theme_to_queue]
+        end
+
+        def job_spec
+          {
+            _pipeline_job_url: 'preparethemepacks',
+            themepack: theme_to_queue
+          }
         end
       end
     end
