dad065ba150df5ed1a4f966a56dab9b5300ad5b7
MichalPisarczyk <m.pisarczyk@livelinktechnology.net>
2018-07-26 17:33:03 +0100

    Update render_themepack.rb

--- app/jobs/render_themepack.rb
+++ app/jobs/render_themepack.rb
@@ -6,12 +6,28 @@ class RenderThemepack
   include Pipeline::Task
 
   output do |map|
-    map.next_task :OutputThemepack
+    map.next_task :OutputThemepack, if: ->(job) { job['render_success'] }
+    map.next_task :RenderThemepack, if: ->(job) { !job['render_success'] && job['render_retries'] <= 3 }
   end
 
   def call
-    puts "Done - #{job_data.inspect}"
-
-    output_next("rendered", {})
+    puts "Attempt to render: #{job_data['themepack']}"
+    
+    cmd = [
+      '/app/web-prism/prism',
+      'tp-cache-web',
+      job_data['themepack']
+    ]
+    system *cmd
+    
+    output_next("rendered", 'render_success' => true, 'render_retries' => render_retries )
+  rescue StandardError => e
+    Pipeline.logger.error e
+    output_next('failed_render', 'render_success' => false, 'render_retries' => render_retries)
+  end
+  
+  def render_retries
+    job_data['render_retries'] ||= 0
+    job_data['render_retries'] += 1
   end
 end
